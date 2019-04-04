ff1487d26ce92d2520da05cba4379a9ed7ddcc46
Michael Cain <michaelcaincb@gmail.com>
2019-04-04 17:19:56 +0000

    object array map used math.sqrt to get pythago result.

---
+++ arrayMapping.js
@@ -0,0 +1,18 @@
+var input = [
+    { x: 3, y: 4 },
+    { x: 12, y: 5 },
+    { x: 8, y: 15 }
+];
+var result = input.map(function (z) {
+    console.log(z.x, z.y)
+    return Math.sqrt(z.x * z.x + z.y * z.y)
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
+
+
+//   Complete the following code for the assertion to pass. 
+// The result should be an array of numbers corresponding to the x-y 
+// pairs provided in the input array (ie: calculate z given x and y).
